11579fb6140e0ad9c9bb6ed934d15f3119a2ed41
vitor-luis <vitorluisdossantosbmx@hotmail.com>
2020-06-23 14:11:29 -0300

    criacao do menu por categorias

--- src/app/Site-Vendas/header-venda/header-venda.component.ts
+++ src/app/Site-Vendas/header-venda/header-venda.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Categorias } from 'src/app/commum/model/categorias.model';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { CategoriasService } from 'src/app/commum/service/categorias.service';
 
 @Component({
   selector: 'app-header-venda',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderVendaComponent implements OnInit {
 
-  constructor() { }
+  private httpReq: Subscription
+
+  categorias: Categorias[] = null
+  statusResponse: number
+  messageApi: string
+  modalRef: BsModalRef
+
+  constructor(
+    private service: CategoriasService
+  ) { }
 
   ngOnInit(): void {
+    this.getCategoriasParaMenu()
+  }
+
+  getCategoriasParaMenu(){
+    this.httpReq = this.service.getAllCategorias().subscribe(response => {
+      this.statusResponse = response.status
+      this.messageApi = response.body['message']
+      console.log(response.body['data'])
+      this.categorias = response.body['data']
+    })
   }
 
 }
